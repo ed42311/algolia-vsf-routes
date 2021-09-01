2e50fba4fb9dd4922f6f83cb09e45b518b48cd8e
Edward Weymouth <14878694+ed42311@users.noreply.github.com>
2021-09-01 12:43:34 -0600

    Update config

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,171 +1,172 @@
-import webpack from 'webpack';
+import webpack from "webpack";
 
 export default {
   server: {
     port: 3002,
-    host: '0.0.0.0'
+    host: "0.0.0.0"
   },
   head: {
-    title: process.env.npm_package_name || '',
+    title: process.env.npm_package_name || "",
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      {
+        hid: "description",
+        name: "description",
+        content: process.env.npm_package_description || ""
+      }
     ],
     link: [
-      { rel: 'icon',
-        type: 'image/x-icon',
-        href: '/favicon.ico'
+      { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+      {
+        rel: "preconnect",
+        href: "https://fonts.gstatic.com",
+        crossorigin: "crossorigin"
       },
       {
-        rel: 'preconnect',
-        href: 'https://fonts.gstatic.com',
-        crossorigin: 'crossorigin'
+        rel: "preload",
+        href:
+          "https://fonts.googleapis.com/css?family=Raleway:300,400,400i,500,600,700|Roboto:300,300i,400,400i,500,700&display=swap",
+        as: "style"
       },
       {
-        rel: 'preload',
-        href: 'https://fonts.googleapis.com/css?family=Raleway:300,400,400i,500,600,700|Roboto:300,300i,400,400i,500,700&display=swap',
-        as: 'style'
-      },
-      {
-        rel: 'stylesheet',
-        href: 'https://fonts.googleapis.com/css?family=Raleway:300,400,400i,500,600,700|Roboto:300,300i,400,400i,500,700&display=swap',
-        media: 'print',
-        onload: 'this.media=\'all\'',
+        rel: "stylesheet",
+        href:
+          "https://fonts.googleapis.com/css?family=Raleway:300,400,400i,500,600,700|Roboto:300,300i,400,400i,500,700&display=swap",
+        media: "print",
+        onload: "this.media='all'",
         once: true
       }
     ],
     script: []
   },
-  loading: { color: '#fff' },
+  loading: { color: "#fff" },
   router: {
-    middleware: ['checkout'],
+    middleware: ["checkout"],
     parseQuery(queryString) {
-      return require('qs').parse(queryString);
+      return require("qs").parse(queryString);
     },
     stringifyQuery(object) {
-      var queryString = require('qs').stringify(object);
-      return queryString ? '?' + queryString : '';
+      var queryString = require("qs").stringify(object);
+      return queryString ? "?" + queryString : "";
     },
     extendRoutes(routes) {
       for (const route of routes) {
-        if (route.path.includes('/Search')) {
-          route.path = '/s';
+        if (route.path.includes("/Search")) {
+          route.path = "/s";
         }
       }
       return routes;
-    },
+    }
   },
   buildModules: [
     // to core
-    '@nuxt/typescript-build',
-    '@nuxtjs/style-resources',
+    "@nuxt/typescript-build",
+    "@nuxtjs/style-resources",
     // to core soon
-    '@nuxtjs/pwa',
-    ['@vue-storefront/nuxt', {
-      coreDevelopment: true,
-      useRawSource: {
-        dev: [
-          '@vue-storefront/commercetools',
-          '@vue-storefront/core'
-        ],
-        prod: [
-          '@vue-storefront/commercetools',
-          '@vue-storefront/core'
-        ]
+    "@nuxtjs/pwa",
+    [
+      "@vue-storefront/nuxt",
+      {
+        coreDevelopment: true,
+        useRawSource: {
+          dev: ["@vue-storefront/commercetools", "@vue-storefront/core"],
+          prod: ["@vue-storefront/commercetools", "@vue-storefront/core"]
+        }
       }
-    }],
-    ['@vue-storefront/nuxt-theme'],
-    ['@vue-storefront/commercetools/nuxt', {
-      i18n: {
-        useNuxtI18nConfig: true
+    ],
+    ["@vue-storefront/nuxt-theme"],
+    [
+      "@vue-storefront/commercetools/nuxt",
+      {
+        i18n: {
+          useNuxtI18nConfig: true
+        }
       }
-    }]
+    ]
   ],
   modules: [
-    'nuxt-i18n',
-    'cookie-universal-nuxt',
-    'vue-scrollto/nuxt',
-    '@vue-storefront/middleware/nuxt'
+    "nuxt-i18n",
+    "cookie-universal-nuxt",
+    "vue-scrollto/nuxt",
+    "@vue-storefront/middleware/nuxt"
   ],
   i18n: {
-    currency: 'USD',
-    country: 'US',
+    currency: "USD",
+    country: "US",
     countries: [
-      { name: 'US',
-        label: 'United States',
-        states: [
-          'California',
-          'Nevada'
-        ]
-      },
-      { name: 'AT',
-        label: 'Austria' },
-      { name: 'DE',
-        label: 'Germany' },
-      { name: 'NL',
-        label: 'Netherlands' }
+      { name: "US", label: "United States", states: ["California", "Nevada"] },
+      { name: "AT", label: "Austria" },
+      { name: "DE", label: "Germany" },
+      { name: "NL", label: "Netherlands" }
     ],
     currencies: [
-      { name: 'EUR',
-        label: 'Euro' },
-      { name: 'USD',
-        label: 'Dollar' }
+      { name: "EUR", label: "Euro" },
+      { name: "USD", label: "Dollar" }
     ],
     locales: [
       {
-        code: 'en',
-        label: 'English',
-        file: 'en.js',
-        iso: 'en'
+        code: "en",
+        label: "English",
+        file: "en.js",
+        iso: "en"
       },
       {
-        code: 'de',
-        label: 'German',
-        file: 'de.js',
-        iso: 'de'
+        code: "de",
+        label: "German",
+        file: "de.js",
+        iso: "de"
       }
     ],
-    defaultLocale: 'en',
+    defaultLocale: "en",
     lazy: true,
     seo: true,
-    langDir: 'lang/',
+    langDir: "lang/",
     vueI18n: {
-      fallbackLocale: 'en',
+      fallbackLocale: "en",
       numberFormats: {
         en: {
           currency: {
-            style: 'currency', currency: 'USD', currencyDisplay: 'symbol'
+            style: "currency",
+            currency: "USD",
+            currencyDisplay: "symbol"
           }
         },
         de: {
           currency: {
-            style: 'currency', currency: 'EUR', currencyDisplay: 'symbol'
+            style: "currency",
+            currency: "EUR",
+            currencyDisplay: "symbol"
           }
         }
       }
     },
     detectBrowserLanguage: {
-      cookieKey: 'vsf-locale'
+      cookieKey: "vsf-locale"
     }
   },
   styleResources: {
-    scss: [require.resolve('@storefront-ui/shared/styles/_helpers.scss', { paths: [process.cwd()] })]
-  },
-  build: {
-    transpile: [
-      'vee-validate/dist/rules',
-      'vue-instantsearch',
-      'instantsearch.js/es'
-    ],
-    plugins: [
-      new webpack.DefinePlugin({
-        'process.VERSION': JSON.stringify({
-          // eslint-disable-next-line global-require
-          version: require('./package.json').version,
-          lastCommit: process.env.LAST_COMMIT || ''
-        })
+    scss: [
+      require.resolve("@storefront-ui/shared/styles/_helpers.scss", {
+        paths: [process.cwd()]
       })
     ]
   },
+  build: {
+    transpile: [
+      "vee-validate/dist/rules",
+      "vue-instantsearch",
+      "instantsearch.js/es"
+      // '@storefront-ui/vue', Does this solve the nesting problem?
+    ],
+    plugins: [
+      new webpack.DefinePlugin({
+        "process.VERSION": JSON.stringify({
+          // eslint-disable-next-line global-require
+          version: require("./package.json").version,
+          lastCommit: process.env.LAST_COMMIT || ""
+        })
+      })
+    ]
+  }
 };
